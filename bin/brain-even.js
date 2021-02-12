6222fc4273045cf492405fd3f479139d448c4b7f
SvetlanaFoglia <luce62006@gmail.com>
2021-02-12 21:19:34 +0100

    brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,19 @@
+
+import readlineSync from 'readline-sync';
+
+function EvenGame(name) {
+  const correct = 0;
+  for (let i = 0; i < 3; i++) {
+    const min = 1;
+    const max = 100;
+    const item = Math.random() * (+max - +min) + min;
+    console.log(`Hello, ${item}!`);
+    const answer = readlineSync.question('Your answer:');
+    const even = item % 2 === 0;
+    const actual = answer === 'yes';
+    if (even === actual) { console.log('Correct!'); }
+  }
+  if (correct === 3) { console.log(`Hello, ${name}!`); }
+}
+
+export default EvenGame;
