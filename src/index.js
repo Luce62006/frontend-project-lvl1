eb5f43b3e23f1d1972dc8d61b8e9137e17832914
SvetlanaFoglia <luce62006@gmail.com>
2021-02-16 18:03:09 +0100

    utils,src,games

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+import getName from '../cli.js';
+
+const roundsNumber = 3;
+
+const engine = (description, getQuestionAndCorrectAnswer) => {
+  // 1-й этап - вывод общеигрового приветствия
+  console.log('Welcome to the Brain Games!');
+  console.log();
+
+  // 2-й этап - вывод описания игры
+  console.log(description);
+  console.log();
+
+  // 3-й этап - запрос имени у пользователя
+  const name = getName();
+
+  // 4-й этап - приветствие пользователя
+  console.log(`Hello, ${name}!`);
+  console.log();
+
+  // 5-й этап - играем раунды
+  for (let i = 1; i <= roundsNumber; i += 1) {
+    const [question, correctAnswer] = getQuestionAndCorrectAnswer();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      console.log();
+    } else {
+      console.log(`"${answer}" is wrong answer! Correct answer is "${correctAnswer}".`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default engine;
