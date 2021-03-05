100b8309273fd5ba93b6298afa2b6943666dbe8a
SvetlanaFoglia <luce62006@gmail.com>
2021-03-05 16:10:35 +0100

    calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,10 +4,11 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What is the result of the expression?';
 
-const getRandomElement = (mathSigns) => mathSigns[getRandomNumber(0, mathSigns.length - 1)];
+// eslint-disable-next-line max-len
+const getRandomElement = (signOfMathOperation) => signOfMathOperation[getRandomNumber(0, signOfMathOperation.length - 1)];
 
-const getMathOperation = (number1, sign, number2) => {
-  switch (sign) {
+const getMathOperation = (number1, signOfMathOperation, number2) => {
+  switch (signOfMathOperation) {
     case '+':
       return number1 + number2;
     case '-':
@@ -24,7 +25,9 @@ const getQuestionAndCorrectAnswer = () => {
   const number1 = getRandomNumber(1, 30);
   const number2 = getRandomNumber(1, 30);
   const mathOperation = getRandomElement(signOfMathOperation);
-  const correctAnswer = getMathOperation(number1, signOfMathOperation, number2);
+  console.log(mathOperation);
+  const correctAnswer = getMathOperation(number1, mathOperation, number2);
+  console.log(correctAnswer);
   const question = `${number1} ${mathOperation} ${number2}`;
   return [question, String(correctAnswer)];
 };
